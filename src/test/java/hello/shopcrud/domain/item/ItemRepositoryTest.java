d93ebd57a30f237e826d316748f2b4bccdbf3f91
sumin <mins19960091@gmail.com>
2025-03-20 23:28:00 +0900

    테스트 코드에 전체조회, 상품등록, 저장 기능 추가

--- src/test/java/hello/shopcrud/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/shopcrud/domain/item/ItemRepositoryTest.java
@@ -3,6 +3,8 @@ package hello.shopcrud.domain.item;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ItemRepositoryTest {
@@ -31,19 +33,36 @@ class ItemRepositoryTest {
     @Test //전체조회
     void findAll() {
         //given
+        Item item1 = new Item("itemA",10000,10);
+        Item item2 = new Item("itemB",20000,20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
 
         //when
+        List<Item> result = itemRepository.findAll();    //결과가 나올때
 
         //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1,item2);
     }
 
 
     @Test //상품등록
     void updateItem() {
         //given
+        Item item = new Item("itemA",10000,10);
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
 
         //when
+        Item updateParam = new Item("item2",20000,30);
+        itemRepository.update(itemId, updateParam);
 
         //then
+        Item findItem = itemRepository.findById(itemId);
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
     }
 }
